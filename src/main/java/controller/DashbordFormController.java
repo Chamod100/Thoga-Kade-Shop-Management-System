4891adbfbfa9d3eaa30a310d490e080856e8d991
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-10-10 23:45:53 +0530

    Add initial project structure with customer management UI and controllers

---
+++ src/main/java/controller/DashbordFormController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class DashbordFormController {
+    public void addItem(ActionEvent actionEvent) {
+    }
+
+    public void updateItem(ActionEvent actionEvent) {
+    }
+
+    public void deleteItem(ActionEvent actionEvent) {
+    }
+
+    public void clearItemFields(ActionEvent actionEvent) {
+    }
+
+    public void addCustomer(ActionEvent actionEvent) {
+    }
+
+    public void updateCustomer(ActionEvent actionEvent) {
+    }
+
+    public void deleteCustomer(ActionEvent actionEvent) {
+    }
+
+    public void clearCustomerFields(ActionEvent actionEvent) {
+        
+    }
+}
---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public void onLogin(ActionEvent actionEvent) {
+    }
+
+    public void onClear(ActionEvent actionEvent) {
+        
+    }
+}
